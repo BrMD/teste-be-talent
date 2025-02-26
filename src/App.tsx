c1bc1811b239a780b772a2ee9c3a065a2160195a
BrMD <dorigon.matheus8@gmail.com>
2025-02-26 14:09:45 -0300

    feat/header created and some default styles colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
+import Main from "./pages/Main";
+
 function App() {
-  return <h1>hello world</h1>;
+  return <Main />;
 }
 
 export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <img className="imgg" src="./img/logo.png" alt="logo" />
+    </header>
+  );
+}
+
+export default Header;
